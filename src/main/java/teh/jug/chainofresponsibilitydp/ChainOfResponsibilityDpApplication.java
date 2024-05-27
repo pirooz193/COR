10ae6e0a58f6abb72b92ab68ebedb1d7c3511be1
Pirooz <p.azarabad@sinacomsys.com>
2024-05-27 10:34:03 +0330

    first commit

---
+++ src/main/java/teh/jug/chainofresponsibilitydp/ChainOfResponsibilityDpApplication.java
@@ -0,0 +1,13 @@
+package teh.jug.chainofresponsibilitydp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainOfResponsibilityDpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainOfResponsibilityDpApplication.class, args);
+	}
+
+}
